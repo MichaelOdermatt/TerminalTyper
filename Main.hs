7ae5161877913cf5952e188793e77f1ea2339d1a
MichaelOdermatt <michaelodermatt@outlook.com>
2023-04-26 21:20:54 -0600

    Reworked the print method
    - separated the print IO function in to multiple pure functions

--- Main.hs
+++ Main.hs
@@ -12,7 +12,7 @@ main = do
 
 mainLoop :: IO ()
 mainLoop = do
-    randomNums <- getListOfRandomInts 10 (length wordBank)
+    randomNums <- getListOfRandomInts 50 (length wordBank)
     let initialWordList = getWordsFromWordBank randomNums
     mainLoop' initialWordList 0
 
@@ -21,7 +21,9 @@ mainLoop' wordList wordIndex = do
     randomNum <- getRandomInt (length wordBank - 1)
     let extendedWordList = wordList ++ [wordBank !! randomNum]
     let currentWord = wordList !! wordIndex
-    printListOfWords extendedWordList wordIndex
+    putStr clearScreen
+    putStrLn ""
+    putStrLn (insertLineBreaks (joinStringsWithSpaces (highlightStringInList extendedWordList currentWord)))
     putStrLn ""
     wordFromUser <- getUserInput
     -- let wasSuccessful = (wordFromList == wordFromUser) will use this later
@@ -57,32 +59,41 @@ getListOfRandomInts x upperRange = do
     nums <- getListOfRandomInts (x - 1) upperRange
     return (num:nums)
 
--- | Prints the list of words to the user, with the first word in the list colored differently
--- TODO: Two issues with this method 1: the output doesn't wrap at spaces which means words get cut off
-printListOfWords :: [String] -> Int -> IO ()
-printListOfWords words highlightedWordIndex = do
-    putStr clearScreen
-    putStrLn ""
-    printListOfWords' words 0 highlightedWordIndex
-
-printListOfWords' :: [String] -> Int -> Int -> IO ()
-printListOfWords' (word:words) currentIndex highlightedWordIndex 
-    | null words = do 
-        return ()
-    | currentIndex == highlightedWordIndex = do
-        putStr textColorCyan
-        putStr (" " ++ word)
-        putStr textColorReset
-        printListOfWords' words (currentIndex + 1) highlightedWordIndex
-    | otherwise = do
-        putStr (" " ++ word)
-        printListOfWords' words (currentIndex + 1) highlightedWordIndex
-
 -------------------- Pure Functions
 
 -- | Takes a List of indexes and returns the Strings at thoes indexes in the wordBank
 getWordsFromWordBank :: [Int] -> [String]
 getWordsFromWordBank = map (wordBank !!)
+
+-- | Returns a the given list with the first occurance of the given word highlighted
+-- TODO fix this since this wont work if the word shows up more than once
+highlightStringInList :: [String] -> String -> [String]
+highlightStringInList [] _ = []
+highlightStringInList (word:words) wordToHighlight
+    | word == wordToHighlight = (textColorCyan ++ word ++ textColorReset) : words
+    | otherwise = word : highlightStringInList words wordToHighlight
+
+{-  |
+    Takes a string and inserts a line break at the next space character after every x 
+    amount of characters (x is the return of the lineCharacterLimit function).
+-}
+insertLineBreaks :: String -> String
+insertLineBreaks x = insertLineBreaks' x 1
+
+insertLineBreaks' :: String -> Int -> String
+insertLineBreaks' [] _ = []
+insertLineBreaks' (x:xs) count
+    | count >= lineCharacterLimit && x == ' ' = '\n' : insertLineBreaks' xs 1
+    | otherwise = x : insertLineBreaks' xs (count + 1)
+
+joinStringsWithSpaces :: [String] -> String
+joinStringsWithSpaces = foldr (\ x y -> x ++ " " ++ y) ""
+
+removeLastCharacter :: String
+removeLastCharacter = "\b \b"
+
+lineCharacterLimit :: Int
+lineCharacterLimit = 50
 
 -------------------- ANSI escape sequences
 -- all excape codes can be found here https://gist.github.com/fnky/458719343aabd01cfb17a3a4f7296797
@@ -94,6 +105,3 @@ textColorCyan = "\ESC[36m"
 
 clearScreen :: String
 clearScreen = "\ESC[2J"
-
-removeLastCharacter :: String
-removeLastCharacter = "\b \b"
